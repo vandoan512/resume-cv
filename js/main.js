6922572ac79e629929b7e36471dbaf69d3250d3d
vandoan <doanguyen512@gmail>
2023-06-02 00:05:47 +0700

    update image for projects

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function loader() {
 function pagepiling() {
   if ($('.js-sections').length) {
     $('.js-sections').pagepiling({
-      anchors: ['home', 'about', 'projects', 'skill', 'contact'],
+      anchors: ['home', 'experience', 'projects', 'skill', 'contact'],
       menu: '#navbar',
       navigation: false,
       onLeave: function onLeave(index, nextIndex, direction) {
